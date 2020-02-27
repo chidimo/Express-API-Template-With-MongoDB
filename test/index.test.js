e400496a317c7324b58f72e92e03ebe468d5cc21
Chidi Orji <orjichidi95@gmail.com>
2020-02-27 01:38:16 +0100

    chore: update tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,15 +1,24 @@
 import { expect, server, BASE_URL } from './setup';
 
-describe('Index page test', () => {
-  it('get base url', done => {
+describe('Test home routes', () => {
+  it('get index page', done => {
     server
-      .get(`${BASE_URL}`)
+      .get(`${BASE_URL}/`)
       .expect(200)
       .end((err, res) => {
         expect(res.status).to.equal(200);
-        expect(res.body.message).to.equal(
-          'Environment variable is coming across.'
-        );
+        expect(res.body.message).to.equal('Index page');
+        done();
+      });
+  });
+
+  it('get about page', done => {
+    server
+      .get(`${BASE_URL}/about`)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.message).to.equal('About page');
         done();
       });
   });
