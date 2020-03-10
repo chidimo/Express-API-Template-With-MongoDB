7c55e34fc132012c0fd3217a83ef5ede78769ce9
Chidi Orji <orjichidi95@gmail.com>
2020-03-11 00:00:49 +0100

    chore: connect to postgres

--- src/settings.js
+++ src/settings.js
@@ -3,3 +3,4 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const testEnvironmentVariable = process.env.TEST_ENV_VARIABLE.trim();
+export const connectionString = process.env.CONNECTION_STRING.trim();
