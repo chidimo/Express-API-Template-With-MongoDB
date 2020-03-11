cb3b21268b51110c066297c898378d566c5f8872
Chidi Orji <orjichidi95@gmail.com>
2020-03-11 02:00:50 +0100

    test: update test

--- test/index.test.js
+++ test/index.test.js
@@ -22,4 +22,19 @@ describe('Test home routes', () => {
         done();
       });
   });
+
+  it('get messages page', done => {
+    server
+      .get(`${BASE_URL}/messages`)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.messages).to.be.instanceOf(Array);
+        res.body.messages.forEach(m => {
+          expect(m).to.have.property('name');
+          expect(m).to.have.property('message');
+        });
+        done();
+      });
+  });
 });
